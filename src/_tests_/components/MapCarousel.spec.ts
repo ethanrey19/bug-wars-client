1928ad36a3698d7b6d484b04c78fdee9d5e3b91c
unknown <ebud6805@gmail.com>
2024-03-15 20:53:13 -0700

    fix carosel tests

--- src/_tests_/components/MapCarousel.spec.ts
+++ src/_tests_/components/MapCarousel.spec.ts
@@ -10,9 +10,9 @@ describe('MapCarousel.vue', () => {
       it('renders gameMaps properly', () => {
         const pinia = createPinia()
         const store = useGameMapStore(pinia)
-        store.setMaps( [{id: 1, name: "Map 1", height: 3,width:3, body: "XXX\nX X\nXXX"},
-        {id: 2, name: "Map 2", height: 4,width:4, body: "XXXX\nX  X\nX XX\nXXXX"},
-        {id: 3, name: "Map 3", height: 2,width:2, body: "XX\nXX"}]);
+        store.setMaps( [{id: 1, name: "Map 1", height: 3,width:3, body: "XXX\nX X\nXXX", imagePath: "src/assets/images/map-0.png"},
+        {id: 2, name: "Map 2", height: 4,width:4, body: "XXXX\nX  X\nX XX\nXXXX", imagePath: "src/assets/images/map-1.png"},
+        {id: 3, name: "Map 3", height: 2,width:2, body: "XX\nXX", imagePath: "src/assets/images/map-2.png"}]);
        const wrapper = mount(MapCarousel,{ global: {
         plugins: [pinia],
       },});
